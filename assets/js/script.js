ffb48e76a2f4c6c14f01615ae6635dd8335298c8
tgtiburon <tg.tiburon@gmail.com>
2021-10-15 20:22:42 -0400

    task name and type are adding to the list

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,29 @@ var createTaskHandler = function() {
     // stop the form from reloading
     event.preventDefault();
 
-    var listItemEl = document.createElement("li"); 
-    listItemEl.className = "task-item"; 
-    listItemEl.textContent = "This is a new task."; 
-    tasksToDoEl.appendChild(listItemEl); 
+    //Using [] selects an html element by one of its attribu
+    var taskNameinput = document.querySelector("input[name='task-name']").value;
+    //console.log(taskNameinput);
+    //console.dir(taskNameinput);
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add html content to div
+    // using innerHTML lets us insert html in a string
+    taskInfoEl.innerHTML = "<h3 class='task-name'> " + taskNameinput + " </h3<span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+
+    
   }; 
 
 formEl.addEventListener("submit", createTaskHandler);
