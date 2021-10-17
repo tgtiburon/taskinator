d9e5b3b083024e154b382968c5200d4c62413a6c
tgtiburon <tg.tiburon@gmail.com>
2021-10-17 15:45:31 -0400

    refactored the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,48 @@ console.log("script.js is loaded");
 var formEl = document.querySelector("#task-form"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() { 
+var taskFormHandler = function(event) { 
 
     // stop the form from reloading
     event.preventDefault();
 
     //Using [] selects an html element by one of its attribu
-    var taskNameinput = document.querySelector("input[name='task-name']").value;
-    //console.log(taskNameinput);
-    //console.dir(taskNameinput);
-    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    //create list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
+    // package up the data as an object
+    let taskDataObj = {
+      name: taskNameInput, 
+      type: taskTypeInput
+    };
 
-    //create div to hold task info and add to list item
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
+    // send it as an argument to createTaskEl
 
-    // add html content to div
-    // using innerHTML lets us insert html in a string
-    taskInfoEl.innerHTML = "<h3 class='task-name'> " + taskNameinput + " </h3<span class='task-type'>" + taskTypeInput + "</span>";
-    listItemEl.appendChild(taskInfoEl);
-
-    //add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);
-
+    createTaskEl(taskDataObj);
     
   }; 
 
-formEl.addEventListener("submit", createTaskHandler);
+  //Creates the new task html code, we pass it taskDataObj for the info.
+  let createTaskEl = function(taskDataObj) {
+
+    // create list item
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold the task info and add it to the list item
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+     // using innerHTML lets us insert html in a string
+    taskInfoEl.innerHTML  = "<h3 class='task-name'> " + taskDataObj.name + " </h3<span class='task-type'>" + taskDataObj.type + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+
+
+
+  }
+
+formEl.addEventListener("submit", taskFormHandler);
 
